1e6fa517cb627d111d0a10ab67a40f9a271d40fa
Anh Huynh <huynhanh8327@gmail.com>
2022-08-03 13:05:59 -0700

    pact filtering

--- src/background.js
+++ src/background.js
@@ -14,26 +14,68 @@ const parseUrl = url => {
 };
 
 //listens for messages sent from contentScripts; debugging purposes
-chrome.runtime.onMessage.addListener(async (message, sender, sendResponse) => {
-    console.log(sender);
+chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
+    console.log(message);
     sendResponse(true);
 
-    //distributing scrapped data each hard-picked sustainable website, then filters for top 5 results
+    const results = [];
+
+    //distributing scrapped metadata each hard-picked sustainable website, then filters for top 5 results
     const toEverlane = async () => {
 
     };
 
     const toPact = async () => {
+        var myHeaders = new Headers();
+        var raw = `blockId=product-grid-1593717194951&gender=${message.gender}&category=&style=&styleCode=&processHeading=false&productColumnSize=col-6+col-md-4&container=%23product-grid-1593717194951+.row.product-list&explodeCards=false&displayExplodedSwatch=false&action=get&pagePath=%2Fmen&options%5BexplodedSwatch%5D=false`;
 
+        myHeaders.append("Accept", "application/json, text/javascript, */*; q=0.01");
+        myHeaders.append("Accept-Language", "en-US,en;q=0.9");
+        myHeaders.append("Connection", "keep-alive");
+        myHeaders.append("Content-Type", "application/x-www-form-urlencoded; charset=UTF-8");
+        myHeaders.append("Origin", "https://wearpact.com");
+        myHeaders.append("Sec-Fetch-Dest", "empty");
+        myHeaders.append("Sec-Fetch-Mode", "cors");
+        myHeaders.append("Sec-Fetch-Site", "same-origin");
+        myHeaders.append("X-Requested-With", "XMLHttpRequest");
+
+        var requestOptions = {
+            method: 'POST',
+            headers: myHeaders,
+            body: raw, 
+            redirect: 'follow'
+        };
+        var result = await fetch("https://wearpact.com/controller/product", requestOptions)
+            .then(response => response.json())
+            .then(result => result.data)
+            .catch(error => console.log('error', error));
+
+        //begin filtering alg.
+        var category;
+        message.color = message.color.split(" ").pop();
+
+        if (!message.type) category = "tops & shirts";
+        else if (message.type === "pullovers") category = "hoodies & sweatshirts";
+        else if (message.waist_line) category = "pants & shorts";
+
+        result = Object.values(result)
+            .filter(e => e.category === category)      //narrows to specific clothing category
+            .map(e => e.packs[Object.keys(e.packs)[0]].default);
+        result = Object.values(Object.assign({}, ...result))      //narrows to each article in a catalog
+            .filter(e => e.primaryColor.map(e => e.color).includes(message.color));        //narrows to color
+            
+        console.log(result);
+
+        return result.splice(0, 5);
     };
 
     const toPatagonia = async () => {
 
     };
 
-    await toEverlane();
-    await toPact();
-    await toPatagonia();
+    toEverlane();
+    toPact();
+    toPatagonia();
 });
 
 //listens for url changes
--- src/contentScripts.js
+++ src/contentScripts.js
@@ -1,22 +1,26 @@
-//set in local storage product data scraped from webpage
+//set in local storage product metadata scraped from webpage
 chrome.storage.local.set({
     variables: {        //variables of item to be filtered
+        gender: document.getElementsByClassName("product-intro__head-name")[0]?.innerText
+            .toLowerCase()
+            .includes("men") ? 
+                "men" : "women",
         name: document.getElementsByClassName("product-intro__head-name")[0]?.innerText,
-        price: document.getElementsByClassName("original")[0]?.innerText,
+        price: document.getElementsByClassName("original")[0]?.innerText.slice(3),
         ...[...document.getElementsByClassName("product-intro__description-table")[0]?.children]
             //parses SHEIN's product description element
             .reduce((acc, curr) => ({
                 ...acc,
-                [curr.children[0].innerText.slice(0, -1).toLowerCase()]: curr.children[1].innerText}
+                [curr.children[0].innerText.slice(0, -1).toLowerCase().replace(/\s/g, "_")]: curr.children[1].innerText.toLowerCase()}
             ), {})
     },
     results: {}
 });
 
-//gets storage data then sends to service worker
+//gets storage metadata then sends to service worker
 chrome.storage.local.get(
     ["variables"],       //if null argument, then gets all data in storage
     result => {
-        chrome.runtime.sendMessage(result);
+        chrome.runtime.sendMessage(result.variables);
     }
 );
